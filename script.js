559c17ebce1713508f5b47d597e6c08fb0a56f94
jysn7 <159090932+jysn7@users.noreply.github.com>
2025-03-06 17:12:46 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//sidebar
 
 function showSidebar(){
     const sidebar = document.querySelector('.navigation-sidebar')
@@ -14,6 +14,7 @@ function showPopup(){
     const popup = document.querySelector('.popup')
     popup.style.opacity ='1';
     popup.style.top = '50%';
+    
     
 }
 function hidePopup(){
@@ -38,9 +39,23 @@ pbtnClear.addEventListener('click', () => {
     pinputs.forEach(input => input.value ='');
     alert("Welcome to the coding club!!! We'll be in touch shortly");
 });
-/*document.querySelector(".register-btn").addEventListener("click",function(){
-    document.querySelector(".popup").classList.add("active");
-});
-document.querySelector(".popup .close-btn-popup").addEventListener("click",function(){
-    document.querySelector(".popup").classList.remove("active");
-});*/
+
+//view more button
+function showMore(){
+    const viewMore = document.querySelector('.view-more-container')
+    viewMore.style.display = 'flex'
+
+    const viewMoreBtn = document.querySelector('.view-more-btn')
+    viewMoreBtn.style.display = 'none'
+    const viewLessBtn = document.querySelector('.view-less-btn')
+    viewLessBtn.style.display = 'flex'
+}
+function showLess(){
+    const viewLess = document.querySelector('.view-more-container')
+    viewLess.style.display = 'none'
+    
+    const viewLessBtn = document.querySelector('.view-less-btn')
+    viewLessBtn.style.display = 'none'
+    const viewMoreBtn = document.querySelector('.view-more-btn')
+    viewMoreBtn.style.display = 'flex'
+}
